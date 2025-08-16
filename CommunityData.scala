f92969054ce1195f3ea3e72997e6ef5c671678f6
kostasada7 <kchristopou@ac.upatras.gr>
2025-08-16 13:25:55 +0300

    Update CommunityData.scala

--- CommunityData.scala
+++ CommunityData.scala
@@ -1,15 +1,14 @@
 package wcc
 
 /**
-  * Created by tariq on 01/05/18.
-  *
+  
   * Statistics about a certain community
   * @param r the size of the community (number of vertices).
-  * @param a number of internal edges.
-  * @param b number of external edges.
+  * @param a contribution of internal edges.
+  * @param b contribution of external edges.
   */
 class CommunityData(val r: Int, val a: Float, val b: Float, val avgCC: Float) extends Serializable {
-  // the edge density δ = 2 * number of edges / squared number of vertices.
+  // the edge density δ = 2 * contribution of edges / squared number of vertices.
   val d: Float = 2 * a / math.pow(r, 2).toFloat
 
   // Override toString method
