432f6726d67aeb44ae9e06a20997bf3b7c92935c
kostasada7 <kchristopou@ac.upatras.gr>
2025-08-22 12:41:34 +0300

    Update GraphXOps.scala

--- GraphXOps.scala
+++ GraphXOps.scala
@@ -7,9 +7,7 @@ import org.apache.spark.sql._
 import org.apache.spark.graphx._
 import scala.reflect.ClassTag
 
-/**
- * Created by tariq on 06/01/18.
- */
+
 object GraphXOps {
   implicit class GXOperations[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]) {
 
@@ -41,9 +39,6 @@ object GraphXOps {
     /**
      * Partition the graph in a way that guarantees all vertices connected from
      * a community C1 to a community C2 will all belong to the same partition.
-     * @param numCommunities
-     * @param cId
-     * @return
      */
     def partitionByCommunity(numCommunities: Int, cId: VD => Long): Graph[VD, ED] = {
       val partitionedEdges = graph.triplets.map({e =>
